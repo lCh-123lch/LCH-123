bb51aecd1cc306bdebc6f30237bcbcbd5585309f
LCH-123 <1728558781@qq.com>
2026-03-03 17:48:16 +0800

    测试

---
+++ app.java
@@ -0,0 +1,11 @@
+package com.fmj;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+@ServletComponentScan
+@SpringBootApplication
+public class app {
+    public static void main(String[] args) {
+        SpringApplication.run(app.class, args);
+    }
+}
